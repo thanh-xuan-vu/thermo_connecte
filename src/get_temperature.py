c80abf4d70edd59b10cb2ade81962694ef129938
Xuan Vu <vuthanh.xuan.k4@gmail.com>
2022-04-12 15:37:35 +0200

    Add additional parameter, debug and test get temperature module

--- src/get_temperature.py
+++ src/get_temperature.py
@@ -6,13 +6,14 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 logger = logging.getLogger(__name__)
 
-def get_config():
+def get_config(address=None):
     port = 1
-    address = 0x76  # TODO: verify this address
+    if not address : 
+        address = 0x77  # TODO: verify this address with i2cdetect -y 1 in terminal
     bus = smbus2.SMBus(port)
 
     calibration_params = bme280.load_calibration_params(bus, address)
-    logger.info('Configuration: ', bus, address, calibration_params)
+    # logger.info('Configuration: ', bus, address, calibration_params)
     return {'bus':bus, 'address':address, 'calibration_params':calibration_params}
 
 def read_bme280(**config) :
@@ -24,17 +25,14 @@ def read_bme280(**config) :
     calibration_params = config['calibration_params']
     data = bme280.sample(bus, address, calibration_params)
     outputs = {'time':data.timestamp, 'temperature':data.temperature}
-    logger.info(data)
+    logger.info(outputs)
     return outputs
 
 if __name__ == '__main__' :
-    # main()
-    # send_secure_mail()
-    # TODO: test with Raspberry Pi
     name = 'Raspberry Pi'
     logger.info(name)
     
     config = get_config()
-    outputs = read_bme280(config)
+    outputs = read_bme280(**config)
 
 
