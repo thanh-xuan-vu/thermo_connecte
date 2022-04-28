c22194172cdf69e6cd97413c1e43b723e813dfc5
Xuan Vu <vuthanh.xuan.k4@gmail.com>
2022-04-28 22:41:46 +0200

    Move parse_opts to update_sheet.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from src import get_temperature
 # from src.send_email import send_secure_gmail
 from src.send_chat import create_message, send_chat
 from src.update_sheet import update_sheet
+from src.update_sheet import parse_opts
 
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s:  %(levelname)s  :%(name)s: %(message)s')
@@ -14,7 +15,11 @@ logger = logging.getLogger(__name__)
 
 # def main_email() :
 
-#     sensor_name, address, max_temperature, pause_time, _ = parse_opts()
+#     opts = parse_opts()      
+    # sensor_name = opts.get('sensor_name', None)
+    # address = opts.get('address', None)
+    # max_temperature = opts.get('max_temperature', None)
+    # pause_time = opts.get('pause_time', None)
 #     # get periodical sensor information 
 #     config = get_temperature.get_config(address)
 #     while True :
@@ -37,13 +42,18 @@ logger = logging.getLogger(__name__)
 #             logger.info(f'De {sender_email}')
 #             logger.info(f'A {receiver_email}')
 #         sleep(pause_time)
-#             # TODO: send data to server 
 
 def main() :
-    sensor_name, address, max_temperature, pause_time, url,_ = parse_opts()
+    # parse parameters
+    opts = parse_opts()
+    sensor_name = opts.get('sensor_name', None)
+    address = opts.get('address', None)
+    max_temperature = opts.get('max_temperature', None)
+    pause_time = opts.get('pause_time', None)
+    webhook = opts.get('groupe_chat_webhook', None)
 
     # check config file
-    if None in [sensor_name, address, max_temperature, pause_time, url] : 
+    if None in [sensor_name, address, max_temperature, pause_time, webhook] : 
         logger.error('Config file not found at token/config.json.')
         return
     # get pi config
@@ -64,7 +74,7 @@ def main() :
 
         # temperature normal, update google sheet
         if temperature < max_temperature and temperature is not None :
-            update_sheet(idx=sensor_name, value=[[sensor_name, str(temperature), str(max_temperature), time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"), 'OK']]) 
+            update_sheet(frigo_id=sensor_name, value=[[sensor_name, str(temperature), str(max_temperature), time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"), 'OK']]) 
 
         # temperature too high, send a chat message, update google sheet
         elif temperature >= max_temperature and temperature is not None :
@@ -72,36 +82,21 @@ def main() :
             message = create_message(sensor_name=sensor_name,
                         sensor_data={'temperature': temperature, 'time': time},
                         max_temperature=max_temperature)
-            send_chat(message, url)
+            send_chat(message, webhook)
             logger.info('Message sent to group chat.')
             # send info to google sheet
-            update_sheet(idx=sensor_name, value=[[sensor_name, str(temperature), str(max_temperature), time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"), 'Température élevée']]) 
+            update_sheet(frigo_id=sensor_name, value=[[sensor_name, str(temperature), str(max_temperature), time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"), 'Température élevée']]) 
 
         # sensor not connected, update google sheet
         elif temperature is None :
             # send chat message
-            logger.warning(f'{time} : No signal from sensor.')
             message = {'text': f'<users/all> *Frigo {sensor_name}* : pas de connexion entre le capteur et le Raspberry pi.'}
+            send_chat(message, webhook)
             logger.info('Message sent to group chat.')
             # send info to google sheet
-            update_sheet(idx=sensor_name, value=[[sensor_name, str(temperature), str(max_temperature), time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"), 'Pas de connexion au capteur']]) 
+            update_sheet(frigo_id=sensor_name, value=[[sensor_name, str(temperature), str(max_temperature), time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"), 'Pas de connexion au capteur']]) 
 
         sleep(pause_time)
-
-def parse_opts(fp='token/config.json'):
-    # parse config 
-    try : 
-        with open(fp, 'r') as fn :
-            opts = json.load(fn)
-            logger.info(opts)
-        sensor_name = opts.get('sensor_name', None)
-        address = opts.get('address', None)
-        max_temperature = opts.get('max_temperature', None)
-        pause_time = opts.get('pause_time', None)
-        url = opts.get('groupe_chat_webhook', None)
-    except FileExistsError as e :
-        logger.error(e)
-    return sensor_name,address,max_temperature,pause_time,url
 
 if __name__ == '__main__' :
     main()
