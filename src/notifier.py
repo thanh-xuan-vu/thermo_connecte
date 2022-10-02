739a8bcf1b10d9aa93b96567bcea96b3d9b8daaf
Xuan Vu <vuthanh.xuan.k4@gmail.com>
2022-10-02 15:38:35 +0200

    Add Notifier abstract class

---
+++ src/notifier.py
@@ -0,0 +1,7 @@
+
+class Notifier() : 
+    def notify_alerte(self) : 
+        raise NotImplementedError("Notifier is an abstract class. Redefine this function in the inherited class instead.")
+
+    def log(self) : 
+        raise NotImplementedError("Notifier is an abstract class. Redefine this function in the inherited class instead.")
