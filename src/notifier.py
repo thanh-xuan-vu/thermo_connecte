063898f027eab5966bce52f241127c8a4bb551ba
Xuan Vu <vuthanh.xuan.k4@gmail.com>
2022-10-17 18:56:08 +0200

    Update parameterss in class Notifier, add AlertLevel

--- src/notifier.py
+++ src/notifier.py
@@ -1,7 +1,12 @@
+from enum import Enum, auto 
+
+class AlertLevel(Enum) :
+    LOW = auto()
+    HIGH = auto()
 
 class Notifier() : 
-    def notify_alerte(self) : 
+    def notify_alert(self, alert_level: AlertLevel, message) : 
         raise NotImplementedError("Notifier is an abstract class. Redefine this function in the inherited class instead.")
 
-    def log(self) : 
+    def log(self, frigo_name, sensor_id, temp, temp_max, date, time, status) : 
         raise NotImplementedError("Notifier is an abstract class. Redefine this function in the inherited class instead.")
